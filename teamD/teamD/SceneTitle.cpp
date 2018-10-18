e2dca28aad113a9c975711cab5e0e719bf5a2667
Hanaeda <c.falcon0818@gmail.com>
2018-10-18 16:02:01 +0900

    gameL追加

---
+++ teamD/teamD/SceneTitle.cpp
@@ -0,0 +1,17 @@
+#include "SceneTitle.h"
+#include "GameHead.h"
+CSceneTitle::CSceneTitle()
+{
+}
+
+CSceneTitle::~CSceneTitle()
+{
+}
+
+void CSceneTitle::InitScene()
+{
+}
+
+void CSceneTitle::Scene()
+{
+}
